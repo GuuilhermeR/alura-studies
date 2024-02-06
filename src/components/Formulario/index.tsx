0a996f1c6f1cc101f1ec77de7cb623d0e9a24b85
GuuilhermeR <guilherme.rudiger@hotmail.com>
2024-02-06 20:25:21 -0300

    Ajuste de espaçamento

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -29,9 +29,7 @@ class Formulario extends React.Component {
                     max="01:30:00"
                     required />
                 <div>
-
                 </div>
-
                 <Botao />
             </form>
         )
