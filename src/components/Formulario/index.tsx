99970783488d3c9b47cf0192c0c5bfa7d9b5f5e7
guilherme.rudiger@movtech.com.br <guilherme.rudiger@movtech.com.br>
2024-02-07 12:57:37 -0300

    Continuação aula.

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Botao from "../Botao";
+import React from 'react';
+import Botao from '../Botao';
+import './style.scss';
 
 class Formulario extends React.Component {
-
     render() {
         return (
-            <form>
-                <div>
+            <form className="novaTarefa">
+                <div className="inputContainer">
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
                     </label>
@@ -14,27 +14,28 @@ class Formulario extends React.Component {
                         type="text"
                         name="tarefa"
                         id="tarefa"
-                        placeholder="O que você quer estudar?"
-                        required />
+                        placeholder="O que você quer estudar"
+                        required
+                    />
                 </div>
-                <label htmlFor="tempo">
-                    Tempo
-                </label>
-                <input
-                    type="time"
-                    step="1"
-                    name="tempo"
-                    id="tempo"
-                    min="00:00:00"
-                    max="01:30:00"
-                    required />
-                <div>
+                <div className="inputContainer">
+                    <label htmlFor="tempo">
+                        Tempo
+                    </label>
+                    <input
+                        type="time"
+                        step="1"
+                        name="tempo"
+                        id="tempo"
+                        min="00:00:00"
+                        max="01:30:00"
+                        required
+                    />
                 </div>
                 <Botao />
             </form>
         )
     }
-
 }
 
 export default Formulario;
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,37 +1,34 @@
-import React from "react";
+import React from 'react';
+import './style.scss';
 
 function Lista() {
-    
     const tarefas = [{
         tarefa: 'React',
         tempo: '02:00:00'
-    },
-    {
-        tarefa: 'JavaScript',
+    }, {
+        tarefa: 'Javascript',
         tempo: '01:00:00'
-    },
-    {
-        tarefa: 'TypeScript',
-        tempo: '03:00:00'
-    }]
+    }, {
+        tarefa: "Typescript",
+        tempo: "03:00:00"
+    }, {
+        tarefa: "C#",
+        tempo: "02:30:00"
+    }];
 
     return (
-        <aside>
+        <aside className="listaTarefas">
             <h2> Estudos do dia </h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index}>
-                        <h3>
-                            {item.tarefa}
-                        </h3>
-                        <span>
-                            {item.tempo}
-                        </span>
+                    <li key={index} className="item">
+                        <h3>{item.tarefa}</h3>
+                        <span>{item.tempo}</span>
                     </li>
                 ))}
             </ul>
         </aside>
-    );
+    )
 }
 
 export default Lista;
