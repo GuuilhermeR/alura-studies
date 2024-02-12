fedb417f3377f210dd12a4a37aaedfe1c08d3296
guilherme.rudiger@movtech.com.br <guilherme.rudiger@movtech.com.br>
2024-02-12 12:58:12 -0300

    Introduzindo estados e Controlando o formulário

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -3,9 +3,19 @@ import Botao from '../Botao';
 import style from './formulario.module.scss';
 
 class Formulario extends React.Component {
+    state = {
+        tarefa: "",
+        tempo: "00:00"
+    }
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
+        evento.preventDefault();
+        console.log('state: ', this.state);
+    }
+
     render() {
         return (
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
@@ -14,6 +24,8 @@ class Formulario extends React.Component {
                         type="text"
                         name="tarefa"
                         id="tarefa"
+                        value={this.state.tarefa}
+                        onChange={e => this.setState({...this.state, tarefa: e.target.value})}
                         placeholder="O que você quer estudar"
                         required
                     />
@@ -26,6 +38,8 @@ class Formulario extends React.Component {
                         type="time"
                         step="1"
                         name="tempo"
+                        value={this.state.tempo}
+                        onChange={e => this.setState({ ...this.state, tempo: e.target.value})}
                         id="tempo"
                         min="00:00:00"
                         max="01:30:00"
