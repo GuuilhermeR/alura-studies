523ae579df108304c335db04e07793c8fbbc7ae3
guilherme.rudiger@movtech.com.br <guilherme.rudiger@movtech.com.br>
2024-02-26 12:39:37 -0300

    Aula 05. States

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import style from './botao.module.scss';
 
-class Botao extends React.Component<{ children: React.ReactNode }> {
-    render() {
-        return (
-            <button className={style.botao}>
-                {this.props.children}
-            </button>
-        )
-    }
+class Botao extends React.Component<{
+  type?: "button" | "submit" | "reset" | undefined;
+  children?: ReactNode;
+}> {
+  render() {
+    const { type = "button" } = this.props;
+    return (
+      <button type={type} className={style.botao}>
+        {this.props.children}
+      </button>
+    )
+  }
 }
+
 
 export default Botao;
--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,13 +1,13 @@
-import style from './relogio.module.scss'
+import style from './relogio.module.scss';
 
 export default function Relogio() {
-    return (
-        <>
-            <span className={style.relogioNumero}>0</span>
-            <span className={style.relogioNumero}>0</span>
-            <span className={style.Divisao}>:</span>
-            <span className={style.relogioNumero}>0</span>
-            <span className={style.relogioNumero}>0</span>
-        </>
-    )
+  return (
+    <>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioDivisao}>:</span>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>0</span>
+    </>
+  )
 }
